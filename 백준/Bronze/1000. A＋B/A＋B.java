dca8268d6e5e9f4382d2c76b2b9f0c9052900de4
kim100000000 <kst980510@gmail.com>
2025-08-31 19:58:16 +0900

    [Bronze V] Title: A+B, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(); // 첫 번째 정수 입력
+        int b = sc.nextInt(); // 두 번째 정수 입력
+        System.out.println(a + b);
+    }
+}
